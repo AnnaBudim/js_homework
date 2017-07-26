454a7759f7c26f3cfe85a36619cdd70746959558
Anna Budim <20anna08@ukr.net>
2017-07-26 13:51:00 +0000

    fixing some error

--- js/index.js
+++ js/index.js
@@ -9,25 +9,28 @@ document.body.style.font;
         let discr = Math.pow(b, 2) - 4 * a * c;
         return discr;
     }
-    let disq = Disqrim(a, b, c);
+    
+     document.write(QuadraticEquation(a, b, c));
+    
+    function QuadraticEquation(a, b, c){
+        let disq = Disqrim(a, b, c);
     document.write('Disqriminator is: ' + disq + '<br>');
-    
-    
         let x1 = ((-b + Math.sqrt(disq))/(2*a));
         let x2 = ((-b - Math.sqrt(disq))/(2*a));
-    function QuadraticEquation(a, b, c){
+        return 'x1 = ' + x1 + ';' + '<br>' + 'x2 = ' + x2;
+        
         if (disq > 0) {
-            return x1 + x2;
+           return x1 + x2;
         } else 
         if (disq === 0) {
-            return x1;
+           return x1;
         } else
         {
-            return 'No roots';
+           return 'No roots';
         }
     }
-     QuadraticEquation(a, b, c);
-     document.write('x1 = ' + x1 + ';' + '<br>' + 'x2 = ' + x2);
+       
+     
 
 
 
